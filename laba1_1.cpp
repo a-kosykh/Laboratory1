0ab942a014379bf1b4636f4711b1e4ccaa5b9be5
Aleksandr Kosykh <kik9399@mail.ru>
2016-10-07 20:12:11 +0300

    Update laba1_1.cpp

--- laba1_1.cpp
+++ laba1_1.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <math.h>
-#include <STDLIB.H>
+#include <stdlib.h>
 using namespace std;
 int main()
 {
-    int x; // Определение переменных
-    double z; // Определение переменных
+    int x=0; // Определение переменных
+    double z=0; // Определение переменных
     // Ввод переменных
-    std::cout << "x = ";
-    std::cin >> x;
-    if (x==-3) std::cout << "Данный x не входит в ОДЗ"; // Вывод сообщения для 'x', который не входит в ОДЗ
+    cout << "x = ";
+    cin >> x;
+    if (x==-3) cout << "Данный x не входит в ОДЗ" << endl; // Вывод сообщения для 'x', который не входит в ОДЗ
     else {
         z = (sqrt((pow(x, 2)) + (2 * x) + 1) / (x + 3)); // Выражение
-        std::cout << "Ответ: " << z; // Вывод ответа
+        cout << "Ответ: " << z << endl; // Вывод ответа
     }
+    return 0;
 }
+
